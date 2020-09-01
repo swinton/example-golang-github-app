c24ff878d09dc62953ed351bd2bf60e88badf364
Steve Winton <stevewinton@gmail.com>
2020-09-01 15:08:03 -0500

    Create comment on issue

--- web/handler.go
+++ web/handler.go
@@ -1,6 +1,7 @@
 package web
 
 import (
+	"context"
 	"encoding/json"
 	"io/ioutil"
 	"log"
@@ -11,6 +12,8 @@ import (
 
 	"github.com/swinton/example-golang-github-app/gh"
 )
+
+var ctx = context.Background()
 
 // HookResponse defines the shape of our HookRouter responses
 type HookResponse struct {
@@ -46,6 +49,8 @@ func HookRouter(app gh.App, path string) *mux.Router {
 		switch e := event.(type) {
 		case *github.IssuesEvent:
 			log.Printf("installation id: %d\n", *e.Installation.ID)
+			log.Printf("issue owner: %s\n", *e.Repo.Owner.Login)
+			log.Printf("issue repo: %s\n", *e.Repo.Name)
 			log.Printf("issue id: %d\n", *e.Issue.ID)
 
 			// Instantiate client
@@ -57,6 +62,17 @@ func HookRouter(app gh.App, path string) *mux.Router {
 				return
 			}
 			log.Printf("client %s instantiated for %s\n", client.UserAgent, client.BaseURL)
+
+			// Create a comment back on the issue
+			// https://github.com/google/go-github/blob/d57a3a84ba041135efb6b7ad3991f827c93c306a/github/issues_comments.go#L101-L117
+			newComment := &github.IssueComment{Body: github.String("## :wave: :earth_americas:\n\n![fellowshipoftheclaps](https://user-images.githubusercontent.com/27806/91333726-91c46f00-e793-11ea-9724-dc2e18ca28d0.gif)")}
+			comment, _, err := client.Issues.CreateComment(ctx, *e.Repo.Owner.Login, *e.Repo.Name, int(*e.Issue.ID), newComment)
+			if err != nil {
+				log.Println(err)
+				http.Error(w, "Server Error", http.StatusInternalServerError)
+				return
+			}
+			log.Printf("comment created: %+v\n", comment)
 		default:
 			log.Printf("Unknown event type: %s\n", github.WebHookType(r))
 			http.Error(w, "Bad Request", http.StatusBadRequest)
